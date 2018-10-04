92a3fa8c36dffd3c1c04c5b8f381a9333c58e11d
vframe <vframe@vframe.io>
2018-10-04 19:37:39 +0200

    import metadta from pkl

--- backend/vsearch/management/commands/import_metadata.py
+++ backend/vsearch/management/commands/import_metadata.py
@@ -14,7 +14,6 @@ class Command(BaseCommand):
   help = 'Import metadata JSON files into the database'
 
   def add_arguments(self, parser):
-    parser.add_argument('--dataset', required=True, type=str)
     parser.add_argument('--ijson', action='store_true')
     parser.add_argument('--unverified', action='store_true', default=False)
 
@@ -23,23 +22,32 @@ class Command(BaseCommand):
     verified = "unverified" if options['unverified'] else "verified"
 
     for tag in DocumentTag.objects.all():
-      filename = "sis/datasets/{}/meta/{}/{}/index.json".format(options['dataset'], tag.name, verified)
+      path = "/data_store/apps/syrianarchive/metadata/{}/{}".format(tag.name, verified)
       if not os.path.exists(filename):
         self.stdout.write("Not found: {}".format(filename))
         continue
       self.stdout.write("Importing {}".format(filename))
-      with open(filename, 'r') as f:
-        data = json.loads(f.read())
-        index = 0
-        for item in data.values():
-          Document.objects.create(
-            tag=tag,
-            sha256=item['sha256'],
-            data=json.dumps(item, separators=(',', ':'))
-          )
-          index += 1
-          if (index % 1000) == 0:
-            self.stdout.write("{}...".format(index))
+      data = load_pickle(path, 'index.pkl')
+      index = 0
+      for item in data.values():
+        Document.objects.create(
+          tag=tag,
+          sha256=item['sha256'],
+          data=json.dumps(item, separators=(',', ':'))
+        )
+        index += 1
+        if (index % 1000) == 0:
+          self.stdout.write("{}...".format(index))
       self.stdout.write(self.style.SUCCESS('Imported {}'.format(filename)))
 
 # if we someday store binary sha256: int(item['sha256'], 16)
+
+def load_pickle(data_dir, pkl_fn):
+  load_start = time.time()
+  with open(path.join(str(data_dir), str(pkl_fn)), 'rb') as fh:
+    raw = fh.read()
+    data = pickle.loads(raw)
+    load_end = time.time()
+    load_time = load_end - load_start
+    print("Pickle load time: {:.1f}s".format(load_time))
+    return data
