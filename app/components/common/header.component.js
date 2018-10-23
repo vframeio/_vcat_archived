3df87d6e53abf9af799efb4168e7c7a7fa374bc8
vframe <vframe@vframe.io>
2018-10-23 18:08:35 +0200

    deploy

--- app/components/common/header.component.js
+++ app/components/common/header.component.js
@@ -31,19 +31,19 @@ function LoggedInHeader(props){
       <section className="navbar-section first-navbar-section">
         <Link to="/"><img className="menuToggle" alt='logo' src="/static/vframe-logo.png" /></Link>
         <Link to="/" className="vcat-btn"><b>VCAT</b></Link>
-        <Link to="/categories/" className="">Categories</Link>
-        <Link to="/images/new/" className="">Upload</Link>
-        <a href="/search/" className="">Search</a>
+        <Link to="/categories/">Categories</Link>
+        <Link to="/images/new/">Upload</Link>
+        <a href="/search/">Search</a>
       </section>
 
       <section className="navbar-section last-navbar-section">
-        <Link to="/static/explore/hierarchy.html" className="">Stats</Link>
-        <Link to="/help/" className="">Help</Link>
+        <Link to="/static/explore/hierarchy.html">Stats</Link>
+        <Link to="/help/">Help</Link>
         <span className="login-btn logged-in capitalize">
           {props.auth.user.username}
           <Link to="/accounts/logout/">Logout</Link>
         </span>
-        <Link to="/groups/user/" className="">My Assignments</Link>
+        <Link to="/groups/user/">My Assignments</Link>
       </section>
     </header>
   )
